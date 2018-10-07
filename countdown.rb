678868d2cb1e3fc72261646fb292d087f9790d37
Dana Palmer <roiai4@hotmail.com>
2018-10-07 01:34:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 def countdown_with_sleep(number)
-counter = 5
+number = 5
 
-while counter > 0
+while number > 0
   puts "#{number} SECONDS!"
   sleep(1)
-  counter -= 1
+  number -= 1
   break
   "HAPPY NEW YEAR!"
 end
